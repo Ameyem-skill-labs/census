08c37f99ca8264b357c94661e16095e58d95a5c5
narunbabu <narun.iitb@gmail.com>
2024-01-05 20:38:55 +0530

    confirm6

--- database/migrations/2023_03_17_165457_create_profile_table.php
+++ database/migrations/2023_03_17_165457_create_profile_table.php
@@ -7,31 +7,31 @@ class CreateProfileTable extends Migration
 {
     public function up()
     {
-        Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->string('gender')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('marriage_status')->nullable();
-            $table->text('about')->nullable();
-            $table->string('mobile')->nullable();
-            $table->date('date_of_birth')->nullable();
-            $table->unsignedBigInteger('education_id')->nullable();
-            $table->unsignedBigInteger('profession_id')->nullable();
-            $table->unsignedBigInteger('role_id')->nullable();
-            $table->unsignedBigInteger('native_place_id')->nullable();
-            $table->unsignedBigInteger('present_place_id')->nullable();
+        // Schema::create('profiles', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->unsignedBigInteger('user_id');
+        //     $table->foreign('user_id')->references('id')->on('users');
+        //     $table->string('gender')->nullable();
+        //     $table->string('avatar')->nullable();
+        //     $table->string('marriage_status')->nullable();
+        //     $table->text('about')->nullable();
+        //     $table->string('mobile')->nullable();
+        //     $table->date('date_of_birth')->nullable();
+        //     $table->unsignedBigInteger('education_id')->nullable();
+        //     $table->unsignedBigInteger('profession_id')->nullable();
+        //     $table->unsignedBigInteger('role_id')->nullable();
+        //     $table->unsignedBigInteger('native_place_id')->nullable();
+        //     $table->unsignedBigInteger('present_place_id')->nullable();
         
-            // Foreign keys
-            $table->foreign('education_id')->references('id')->on('qualifications')->onDelete('set null');
-            $table->foreign('profession_id')->references('id')->on('professions')->onDelete('set null');
+        //     // Foreign keys
+        //     $table->foreign('education_id')->references('id')->on('qualifications')->onDelete('set null');
+        //     $table->foreign('profession_id')->references('id')->on('professions')->onDelete('set null');
 
-            $table->foreign('native_place_id')->references('id')->on('villages')->onDelete('set null');
-            $table->foreign('present_place_id')->references('id')->on('villages')->onDelete('set null');
+        //     $table->foreign('native_place_id')->references('id')->on('villages')->onDelete('set null');
+        //     $table->foreign('present_place_id')->references('id')->on('villages')->onDelete('set null');
         
-            $table->timestamps();
-        });
+        //     $table->timestamps();
+        // });
         
         // Schema::create('genders', function (Blueprint $table) {
         //     $table->id();
